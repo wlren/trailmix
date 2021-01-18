240625611001a4163758b95d4bc799ea044b8681
Frozenwolf23 <nzixuan@gmail.com>
2021-01-18 14:49:19 +0800

    Google maps integration

--- lib/data/attraction_data.dart
+++ lib/data/attraction_data.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../models/attraction.dart';
+import '../models/attraction.dart';
 
-const attractionData = const [];
+const attractionData = const [
+  AttractionItem(
+      description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      titleID: "Hello World",
+      imageUrl: null,
+      location: LatLng(0, 0))
+];
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../data/attraction_data.dart';
 import '../widgets/app_drawer.dart';
 
 class MapScreen extends StatefulWidget {
@@ -8,7 +9,41 @@ class MapScreen extends StatefulWidget {
   _MapScreenState createState() => _MapScreenState();
 }
 
+class AttractionCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card();
+  }
+}
+
 class _MapScreenState extends State<MapScreen> {
+  GoogleMapController mapController;
+  OverlayEntry overlayEntry;
+
+  final LatLng _center = const LatLng(45.0, 0);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  void _showCard() {
+    overlayEntry = OverlayEntry(
+      builder: (BuildContext context) {
+        return Positioned(
+          top: 0.0,
+          left: 0.0,
+          child: Card(
+            child: Text("Hi"),
+          ),
+        );
+      },
+    );
+    Overlay.of(context).insert(overlayEntry);
+  }
+
+  static final CameraPosition _myLocation = CameraPosition(
+    target: LatLng(0, 0),
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +51,26 @@ class _MapScreenState extends State<MapScreen> {
         title: const Text("Sungei Buloh Map"),
       ),
       drawer: AppDrawer(),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+        markers: attractionData
+            .map(
+              (attraction) => Marker(
+                markerId: MarkerId(attraction.titleID),
+                icon: BitmapDescriptor.defaultMarker,
+                position: attraction.location,
+                infoWindow: InfoWindow(
+                  title: attraction.titleID,
+                  onTap: _showCard,
+                ),
+              ),
+            )
+            .toSet(),
+      ),
     );
   }
 }
