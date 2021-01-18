eda5b25b4bfd5450f6692670e7c7cf66f09f86f2
Frozenwolf23 <nzixuan@gmail.com>
2021-01-18 15:45:34 +0800

    Change default map location

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../data/attraction_data.dart';
+import '../models/attraction.dart';
+import '../models/attraction.dart';
 import '../widgets/app_drawer.dart';
 
 class MapScreen extends StatefulWidget {
@@ -19,26 +21,20 @@ class AttractionCard extends StatelessWidget {
 class _MapScreenState extends State<MapScreen> {
   GoogleMapController mapController;
   OverlayEntry overlayEntry;
+  bool _cardVisable = false;
+  AttractionItem cardAttraction;
 
-  final LatLng _center = const LatLng(45.0, 0);
+  final LatLng _center = const LatLng(1.4466672, 103.7275178);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
-  void _showCard() {
-    overlayEntry = OverlayEntry(
-      builder: (BuildContext context) {
-        return Positioned(
-          top: 0.0,
-          left: 0.0,
-          child: Card(
-            child: Text("Hi"),
-          ),
-        );
-      },
-    );
-    Overlay.of(context).insert(overlayEntry);
+  void _displayInfo(AttractionItem attraction) {
+    setState(() {
+      _cardVisable = true;
+      cardAttraction = attraction;
+    });
   }
 
   static final CameraPosition _myLocation = CameraPosition(
@@ -47,30 +43,50 @@ class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Sungei Buloh Map"),
-      ),
-      drawer: AppDrawer(),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
+        appBar: AppBar(
+          title: const Text("Sungei Buloh Map"),
         ),
-        markers: attractionData
-            .map(
-              (attraction) => Marker(
-                markerId: MarkerId(attraction.titleID),
-                icon: BitmapDescriptor.defaultMarker,
-                position: attraction.location,
-                infoWindow: InfoWindow(
-                  title: attraction.titleID,
-                  onTap: _showCard,
+        drawer: AppDrawer(),
+        body: new Stack(
+          children: [
+            GoogleMap(
+              onTap: (LatLng) => this.setState(() {
+                _cardVisable = false;
+              }),
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 16.2,
+              ),
+              markers: attractionData
+                  .map(
+                    (attraction) => Marker(
+                      markerId: MarkerId(attraction.titleID),
+                      icon: BitmapDescriptor.defaultMarker,
+                      position: attraction.location,
+                      onTap: () => _displayInfo(attraction),
+                    ),
+                  )
+                  .toSet(),
+            ),
+            if (_cardVisable)
+              new Container(
+                alignment: Alignment.topCenter,
+                padding: new EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * .58,
+                    right: 20.0,
+                    left: 20.0),
+                child: new Container(
+                  height: 200.0,
+                  width: MediaQuery.of(context).size.width,
+                  child: new Card(
+                    color: Colors.white,
+                    elevation: 4.0,
+                    child: Text(cardAttraction.titleID),
+                  ),
                 ),
               ),
-            )
-            .toSet(),
-      ),
-    );
+          ],
+        ));
   }
 }
